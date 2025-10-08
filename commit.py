1d60651cc77b197d350c48c7c7d79e5ae15d1211
Nguyen Tran <me@mac.local>
2025-10-07 19:55:23 -0500

    update commit.py

--- commit.py
+++ commit.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import shutil
-os.system("git add .")
-os.system(f"git commit -m \"{sys.argv[1]}\"")
-os.system("git push")
+if sys.argv[1] != "false":
+    os.system("git add .")
+    os.system(f"git commit -m \"{sys.argv[1]}\"")
+    os.system("git push")
 shutil.copy(os.getcwd() + "/engine.py", os.getcwd() + "/lichess-bot")
