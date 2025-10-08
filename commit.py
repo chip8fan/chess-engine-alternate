6b8149fd75e465eea126de526f84d3ac742baf14
Nguyen Tran <me@mac.local>
2025-10-07 19:43:17 -0500

    add gitignore

--- commit.py
+++ commit.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import shutil
 os.system("git add .")
 os.system(f"git commit -m \"{sys.argv[1]}\"")
 os.system("git push")
+shutil.copy(os.getcwd() + "/engine.py", os.getcwd() + "/lichess-bot")
